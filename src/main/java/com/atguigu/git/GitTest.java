4b8bef02f54ff55d3b4cc041e42ec9911843623e
Fang <12345678@qq.com>
2022-09-27 17:46:56 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,9 +1,13 @@
 package com.atguigu.git;
 
+/**
+ *
+ */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,Git！！");
         System.out.println("hello,Git2!!");
         System.out.println("hello,git3！！");
+        System.out.println("Hello,git4！！");
     }
 }
